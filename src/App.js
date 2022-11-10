83caab5865b5e912fe7ed09dface51edbadf8f02
Gokul0201 <kgokula314@gmail.com>
2022-11-10 19:58:52 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
   }
   return (
     <div className="App">
-     <h2>Razorpay Payment Integration Using React</h2>
+     <h2>Razorpay Payment Gateway Using React</h2>
      <br/>
      <input type="text"placeholder='Enter Amount'value={amount}onChange={(e)=>setamount(e.target.value)} />
      <br/><br/>
